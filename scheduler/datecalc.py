b7cdc9b06592f11d5e4e5bdb88c6bd46484bcc57
Jure Ziberna <j.ziberna@gmail.com>
2012-01-15 13:22:21 +0100

    copyright: old nickname -> real name

--- scheduler/datecalc.py
+++ scheduler/datecalc.py
@@ -1,5 +1,5 @@
 #    py-scheduler
-#    Copyright (C) 2011 Kantist
+#    Copyright (C) 2011 Jure Žiberna
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
